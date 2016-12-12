3fc56b0a4cf8dc6e79a3706d47241209a374b4f4
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-11 19:44:42 -0500

    Creates our first component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react' // This imports the react package from our dependencies
+import { render } from 'react-dom' // This only imports the render function from the react-dom package
+
+class StorePicker extends React.Component {
+	render(){
+		return <p>Hello</p>
+	}
+}
+
+render(<StorePicker/>, document.querySelector('#main'));
