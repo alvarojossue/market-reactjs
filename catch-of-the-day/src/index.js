51cdeec545a5712e099b7e3dcb016e326f9febfa
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-11 19:49:29 -0500

    Refactors and puts the StorePicker component in its own file

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,10 +2,6 @@
 import React from 'react' // This imports the react package from our dependencies
 import { render } from 'react-dom' // This only imports the render function from the react-dom package
 
-class StorePicker extends React.Component {
-	render(){
-		return <p>Hello</p>
-	}
-}
+import StorePicker from './components/StorePicker'
 
 render(<StorePicker/>, document.querySelector('#main'));
