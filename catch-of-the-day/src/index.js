c2ee8e4b99aceee6fde272fe38f6e360e6e1c3ec
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-16 16:45:32 -0500

    Creates components for App, Header, Inventory, Order

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,8 @@
 // let's go!
 import React from 'react' // This imports the react package from our dependencies
 import { render } from 'react-dom' // This only imports the render function from the react-dom package
-
+import './css/style.css'
+import App from './components/App'
 import StorePicker from './components/StorePicker'
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
