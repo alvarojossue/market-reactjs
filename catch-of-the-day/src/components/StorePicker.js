838a32933f8965c8651ef4c8a4bc69d6abcf441a
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-16 21:29:11 -0500

    Creates a method to redirect to the store url when the form is submitted

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,15 +2,26 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+
+	goToStore(event){
+		event.preventDefault(); // Prevents button from submitting and redirecting
+		const storeId = this.storeInput.value; // Gets the text from the box
+		this.context.router.transitionTo(`/store/${storeId}`); // Transition to the store url
+	}
+
 	render(){
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}> {/* On submit, execute the goToStore function */}
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/> {/* To be able to access the input text through storeInput from the goToStore function */}
 				<button type="submit"> Visit Store </button>
 			</form>
 		)
 	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
